4556eb16bc5761e0d16dfffcc5f96efd53ab19cc
ElMaikina <mikesoto890@gmail.com>
2025-09-25 12:25:21 -0300

    quitar scripts de usuarios y quitar comando jq

--- app/scripts/authenticate.sh
+++
@@ -1,8 +0,0 @@
-curl -H 'Content-Type: application/json' \
-      -d '{
-		"NombreDeUsuario":"ElMaikina",
-		"Contrasena":"asoqe123135",
-		"Correo": "elmaikina@gmail.com"
-	}' \
-      -X POST \
-      localhost:8080/usuario/autenticar | jq '.'
--- app/scripts/benchmark.sh
+++ app/scripts/benchmark.sh
@@ -1,5 +1,4 @@
 # Mide que tanto tiempo tarda en enviar un flujo masivo de datos por segundo
 time for i in {1..1000}; do 
-	curl -X GET "localhost:8080/instrumento" \
-	--compressed --insecure -s -o /dev/null -w "%{time_total}s\n"
+	curl -X GET "localhost:8080/instrumentos" --compressed --insecure -s -o /dev/null -w "%{time_total}s\n"
 done
